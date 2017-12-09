f839bb5035883274f0eab4a8cfdd0bf57ccda18a
_OMEGA <o_memaster@hotmail.com>
2017-12-10 02:27:04 +0700

    update actual model (subject, student, lecturer)

--- log/student/models.py
+++ log/student/models.py
@@ -1,39 +1,47 @@
 from django.db import models
 from django.urls import reverse
+import datetime
 
 # Create your models here.
 
 class Department(models.Model):
-    DepName = models.CharField(max_length = 50)
-    DepHead = models.CharField(max_length = 100)
-    DepTel = models.CharField(max_length=20)
-    DepEmail = models.CharField(max_length = 500)
+    DepNo = models.CharField(max_length = 4, default='xxxx')
+    DepName = models.CharField(max_length = 30)
+    DepTel = models.CharField(max_length=12)
+    DepEmail = models.CharField(max_length = 50)
+    DepHead = models.CharField(max_length = 11)
     Dep_logo = models.ImageField()
 
     def get_absolute_url(self):
         return reverse('student:detail', kwargs = {'pk': self.pk})
 
     def __str__(self):
-        return self.DepName
+        return self.DepNo + '--' + self.DepName
 
 class Student(models.Model):
     department = models.ForeignKey(Department, on_delete = models.CASCADE, default='DEP')
-    StudentID = models.CharField(max_length=10, default='xxxxxxxxxx')
-    name = models.CharField(max_length = 200, default='NAME')
-    Degree = models.CharField(max_length = 200, default='DEG')
-    DOB = models.CharField(max_length = 200, default='DEF')
-    Tel = models.CharField(max_length = 200, default='TEL')
-    status = models.BooleanField(default=False)
-
+    StudentID = models.CharField(max_length=11)
+    Degree = models.CharField(max_length = 20, default='DEG')
+    name = models.CharField(max_length = 50, default='NAME')
+    DOB = models.DateField()
+    Tel = models.CharField(max_length = 12, default='TEL', blank=True, null=True)
+    Email = models.CharField(max_length = 12, default=None, blank=True, null=True)
+    GPAX = models.FloatField(null=True)
+    Address = models.CharField(max_length = 100, default=None, blank=True, null=True)
+    Status = models.CharField(max_length = 20, default='Normal')
+    CurrentSemester = models.CharField(max_length = 20, default='X/YYYY')
+    Curriculum = models.CharField(max_length = 20, default='CURRICULUM NAME')
 
     def __str__(self):
         return self.name
 
 class Subject(models.Model):
     subjectFK = models.ForeignKey(Student, on_delete=models.CASCADE)
-    SubjectID = models.CharField(max_length=10)
+    SubjectID = models.CharField(max_length=8)
     SubjectName = models.CharField(max_length=30)
-    Section = models.CharField(max_length=2)
+    Prerequisite = models.CharField(max_length=8, default=None, blank=True, null=True)
+    Semester = models.CharField(max_length = 20, default='X/YYYY')
+    Credit = models.IntegerField(default=3)
 
     def __str__(self):
         return self.SubjectName
